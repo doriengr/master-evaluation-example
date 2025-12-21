7aa7ddd382ae6aa9243567694ec4b98e867ad8a9
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2025-12-21 16:29:51 +0100

    feat: add functionality for navigation

--- src/assets/js/components/navigation.js
+++ src/assets/js/components/navigation.js
@@ -1,6 +1,20 @@
 let button = document.querySelector('.navigation__button');
-let navigation = document.querySelector('.navigation__list');
+let subNavButtons = document.querySelectorAll('.navigation__sublist-button');
+let navigation = document.querySelector('.navigation__wrapper');
 
-button.addEventListener('click', () => {
-    navigation.classList.toggle('navigation__list--open');
+if (button) {
+    button.addEventListener('click', () => {
+        navigation.classList.toggle('navigation__wrapper--open');
+    })
+}
+
+subNavButtons.forEach(subNavButton => {
+    const parent = subNavButton.parentElement;
+    const subNavigation = parent.querySelector('.navigation__sublist');
+    if (!subNavigation) return;
+    
+    subNavButton.addEventListener('click', () => {
+        subNavigation.classList.toggle('navigation__sublist--open');
+        subNavButton.classList.toggle('navigation__sublist-button--open');
+    });
 })
